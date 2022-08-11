2ec54f9f5f711d60ec3d6acbddb47bdcf779396c
Daniil Gubanov <yranidrop@gmail.com>
2022-08-11 18:49:44 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Solution
+{
+    class Solution
+    {
+        public static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+            Console.WriteLine(MultiplicationTable(size));
+        }
+        public static int[,] MultiplicationTable(int size)
+        {
+            int[,] arr = new int[size, size];
+
+            for (int i = size - 1; i >= 0; i--)
+                arr[0, i] = i + 1;
+
+            for (int i = size - 1; i >= 0; i--)
+                arr[i, 0] = i + 1;
+
+            for (int i = 1; i < size; i++)
+                for (int j = 1; j < size; j++)
+                    arr[i, j] = arr[0, j] * arr[i, 0];
+            return arr;
+        }
+    }
+}
